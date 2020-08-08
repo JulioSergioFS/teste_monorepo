1169e95cc2fe9d36dfc63602d847f0831eee01d7
Julio Sergio <juliosergio010302@gmail.com>
2020-08-08 19:48:44 -0300

    Practicing with double variables

---
+++ packages/java_Alura_Course_part1/Practice2.java
@@ -0,0 +1,34 @@
+
+public class Practice2 {
+	public static void main(String[] args) {
+		double floatingNumber = 3.14;
+		System.out.println(floatingNumber);
+		
+		double a = 3, b = 2;
+		System.out.println("I can put the values " + a + " and " + b + " at the same line!");
+		
+		double c = 20.6;
+		int d = 2;
+		System.out.println(c/d);
+		System.out.println(d/c);
+		
+		c = 20;
+		System.out.println(c/d);
+		
+		int e = 3;
+		int f = 2;
+		
+		System.out.println(e/f);
+		
+		int g = e/f;
+		System.out.println(g);
+		
+		c = 27;
+		System.out.println(e/c);
+		System.out.println(c/e);
+		
+		double h = 5/2;
+		double i = 5.0/2;
+		System.out.println(h + " and " + i);
+	}
+}
