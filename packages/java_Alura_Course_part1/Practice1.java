df003e52033327e96676491f65ef5fe8ecd112c7
Julio Sergio <juliosergio010302@gmail.com>
2020-08-08 19:16:25 -0300

    Practicing with int variables

---
+++ packages/java_Alura_Course_part1/Practice1.java
@@ -0,0 +1,14 @@
+
+public class Practice1 {
+	public static void main(String[] args) {
+		int number;
+		number = 10;
+		System.out.println(number);
+		
+		int another_number = 20;
+		System.out.println("The fist number is " + number + " and the second one is " + another_number);
+		
+		number = 99;
+		System.out.println("Now the value of number is " + number);
+	}
+}
